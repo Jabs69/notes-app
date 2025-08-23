2fd66ee43a45278eb5ec190525b951226c68f2eb
Jabs69 <jbenitezsaez@gmail.com>
2025-08-23 01:51:54 -0400

    more typescript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 
 	plugins:[react()],
-	test:{
+	// test:{
 
-		environment:'happy-dom'
+	// 	environment:'happy-dom'
 
-	},
+	// },
 });
